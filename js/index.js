fb128f518994217040ec4eae287585fd4aa4a44b
Kineticske <developverphy@gmail.com>
2021-11-11 13:28:11 -0500

    con reloj

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import HamburguerMenu from "./menu-hamb.js";
-
+import { digitalClock, alarm } from "./reloj.js";
 const D=document;
 
 D.addEventListener("DOMContentLoaded", (e)=>{
-    HamburguerMenu(".panel-btn", ".panel", ".menu a")
+    HamburguerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj" );
 });
